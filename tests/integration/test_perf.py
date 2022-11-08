2eab502cc2205a5a70caaf67ba09438b49e11b33
Bradley McElroy <bradley.mcelroy@live.com>
2022-11-08 19:03:04 +1100

    Refine perf test

--- tests/integration/test_perf.py
+++ tests/integration/test_perf.py
@@ -1,14 +1,16 @@
-from betfair_parser.core import read_file
+import fsspec
+
+from betfair_parser.core import parse
 from tests.unit.conftest import RESOURCES_DIR
 
 
-def run():
-    fn = RESOURCES_DIR / "data/27312315.bz2"
-    return list(read_file(fn))
+def run(lines: list[bytes]):
+    return [parse(line) for line in lines]
 
 
 def test_performance(benchmark):
-    # benchmark something
-    result = benchmark.pedantic(run, rounds=1, iterations=1)
-
+    fn = RESOURCES_DIR / "data/27312315.bz2"
+    with fsspec.open(fn, compression="infer") as f:
+        lines = f.read().split(b"\n")[:-1]
+    result = benchmark.pedantic(run, args=(lines,), rounds=1, iterations=1)
     assert len(result) == 50854
