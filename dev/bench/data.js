7beb25d44ce3aa8c7a4003df2973fe5f6b73e39e
github-action-benchmark <github@users.noreply.github.com>
2023-02-17 06:21:39 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for a6bef859b1473bec2a5490220e79df441a032aab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676614723396,
+  "lastUpdate": 1676614899147,
   "repoUrl": "https://github.com/limx0/betfair_parser",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000005638946108474067",
             "extra": "mean: 49.63612000096873 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bradley.mcelroy@live.com",
+            "name": "Bradley McElroy",
+            "username": "limx0"
+          },
+          "committer": {
+            "email": "bradley.mcelroy@live.com",
+            "name": "Bradley McElroy",
+            "username": "limx0"
+          },
+          "distinct": true,
+          "id": "a6bef859b1473bec2a5490220e79df441a032aab",
+          "message": "WIP",
+          "timestamp": "2023-02-17T17:20:39+11:00",
+          "tree_id": "a56ea891e826b9c353520ce473f62250c2e67c3b",
+          "url": "https://github.com/limx0/betfair_parser/commit/a6bef859b1473bec2a5490220e79df441a032aab"
+        },
+        "date": 1676614898385,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/integration/benchmark.py::test_performance",
+            "value": 0.12443914463989912,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 8.036056522999985 sec\nrounds: 1"
+          },
+          {
+            "name": "tests/integration/benchmark.py::test_market_update_performance",
+            "value": 575043.4258376253,
+            "unit": "iter/sec",
+            "range": "stddev: 3.0522191408501754e-8",
+            "extra": "mean: 1.7389991000129612 usec\nrounds: 100"
+          },
+          {
+            "name": "tests/integration/benchmark.py::test_market_definition_performance",
+            "value": 20671.544050161887,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000005309317924902622",
+            "extra": "mean: 48.37567999629755 usec\nrounds: 5"
           }
         ]
       }
