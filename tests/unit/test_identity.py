8f290679234829257f4a3cabc92622eacdf8314f
Michael Löffler <michael@loeffler.io>
2023-11-06 00:53:51 +0200

    Fix CertLogin

--- tests/unit/test_identity.py
+++ tests/unit/test_identity.py
@@ -2,7 +2,7 @@ import pytest
 
 from betfair_parser.endpoints import ENDPOINTS
 from betfair_parser.spec.common import first_lower
-from betfair_parser.spec.identity import KeepAlive, LoginResponse, Logout
+from betfair_parser.spec.identity import CertLogin, KeepAlive, LoginResponse, Logout
 
 
 @pytest.mark.parametrize("msg", [KeepAlive(), KeepAlive.with_params(), Logout(), Logout.with_params()])
@@ -14,10 +14,17 @@ def test_request_init(msg):
     """
     assert msg.method == first_lower(type(msg).__name__)
     assert not msg.body()
-    assert ENDPOINTS.url_for_request(msg).lower().endswith(f"/api/{type(msg).__name__.lower()}")
+    assert ENDPOINTS.url_for_request(msg).endswith(f"/api/{first_lower(type(msg).__name__)}")
     assert msg.validate()
 
 
 def test_login_response():
     resp = b'{"token":"","product":"","status":"FAIL","error":"INPUT_VALIDATION_ERROR"}'
     assert LoginResponse.parse(resp)
+
+
+def test_certlogin():
+    """CertLogin is the only method, that doesn't use camelCase for the method name."""
+    msg = CertLogin.with_params(username="asdf", password="test")
+    assert msg.method == type(msg).__name__.lower()
+    assert ENDPOINTS.url_for_request(msg).endswith(f"/api/{type(msg).__name__.lower()}")
